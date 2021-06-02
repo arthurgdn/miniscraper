17559dab3df7843e8e0a0d5b0177cb8eea312eff
Arthur Guédon <arthur.guedon@student-cs.fr>
2021-06-02 10:29:06 +0200

    replace axios with node-fetch

--- src/builder.ts
+++ src/builder.ts
@@ -1,13 +1,14 @@
-import axios from 'axios';
 import { JSDOM } from 'jsdom';
+import fetch from 'node-fetch';
 
 export const buildDocument = async (url: string): Promise<Element> => {
   try {
-    const response = await axios.get(url);
+    const response = await (await fetch(url)).text();
+    console.log(response);
     if (!response) {
       throw new Error('Unable to fetch corresponding URL');
     }
-    const dom = new JSDOM(response.data);
+    const dom = new JSDOM(response);
     return dom.window.document as unknown as Element;
   } catch (e) {
     throw new Error('Error while building document');
