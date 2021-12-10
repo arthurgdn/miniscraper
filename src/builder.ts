6369c94b828ad93d09f76134ab6aba050ec9be7c
Arthur Guédon <arthur@lamafactory.com>
2021-12-10 11:04:38 +0100

    Use puppeteer for SSR of scraped content

--- src/builder.ts
+++ src/builder.ts
@@ -1,20 +1,15 @@
 import { JSDOM } from "jsdom";
 import fetch from "node-fetch";
+import puppeteer from "puppeteer";
 
 export const buildDocument = async (url: string): Promise<Element> => {
   try {
-    const response = await (
-      await fetch(url, {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0",
-        },
-      })
-    ).text();
-    if (!response) {
-      throw new Error("Unable to fetch corresponding URL");
-    }
-    const dom = new JSDOM(response);
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle2" });
+    const html = await page.content(); // serialized HTML of page DOM.
+    await browser.close();
+    const dom = new JSDOM(html);
     return dom.window.document as unknown as Element;
   } catch (e) {
     throw new Error("Error while building document");
