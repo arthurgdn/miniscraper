46a8dce23d5206c3a4c5a1acaffa0c530fffed1c
arthurgdn <arthurguedon0@gmail.com>
2023-03-17 15:25:41 +0100

    Handle image src extraction

--- src/formatters.ts
+++ src/formatters.ts
@@ -1,3 +1,5 @@
 export const extractText = (nodesArray: Element[]): string[] => {
-  return nodesArray.map(node => node.textContent || '');
+  return nodesArray.map(
+    (node) => (node as any).textContent || (node as any).src || ""
+  );
 };
